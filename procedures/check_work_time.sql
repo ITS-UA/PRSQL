28de97ed152ccb1b973668763a31b4b6fec8bd71
Alex Stupak <itskills4practice@gmail.com>
2023-09-06 16:11:13 +0300

    PRSQL1-30: added fix code

--- procedures/check_work_time.sql
+++ procedures/check_work_time.sql
@@ -1,9 +1,12 @@
 CREATE PROCEDURE check_work_time IS
 BEGIN
     CASE 
-    WHEN TO_CHAR(SYSDATE, 'DY') IN ('SAT', 'SUN') THEN
-      raise_application_error (20001,'Ви не можете вносити зміни в суботу та неділю');
+    WHEN TO_CHAR(SYSDATE, 'DY') IN ('СБ.', 'НД.') THEN
+      raise_application_error (-20001,'Ви не можете вносити зміни в суботу та неділю');
     WHEN TO_CHAR (SYSDATE, 'HH24:MI') NOT BETWEEN '08:00' AND '18:00' THEN
-        raise_application_error (20002,'Ви можете вносити зміни тільки з 8 по 18 годину');
+        raise_application_error (-20002,'Ви можете вносити зміни тільки з 8 по 18 годину');
+    ELSE
+      NULL;
     END CASE;
 END check_work_time;
+/
