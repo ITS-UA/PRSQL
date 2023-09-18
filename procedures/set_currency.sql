6e200a87f30ffe2a6738d8ddb2a3e4fa1775d97e
Alex Stupak <itskills4practice@gmail.com>
2023-09-18 17:49:40 +0300

    PRSQL1-77: update set_currency

--- procedures/set_currency.sql
+++ procedures/set_currency.sql
@@ -3,15 +3,16 @@ begin
 
     update srv.currency_history
     set end_date = sysdate
-    where end_date is null;
-    
+    where end_date = TO_DATE('31.12.2999 23:59:59', 'DD.MM.YYYY HH24:MI:SS');
+
     execute immediate 'truncate table srv.currency';
+    
     
     insert all
         into srv.currency (currency_id, currency_txt, currency_value,  currency_date) 
         values (cur_id, cur_txt, cur_value, cur_date)
         into srv.currency_history (currency_id, currency_txt, currency_value, start_date, end_date, currency_date)
-        values (cur_id, cur_txt, cur_value, sysdate, null, cur_date)   
+        values (cur_id, cur_txt, cur_value, sysdate, TO_DATE('31.12.2999 23:59:59', 'DD.MM.YYYY HH24:MI:SS'), cur_date)   
     select cur.r030 as cur_id, 
            cur.cc as cur_txt, 
            cur.rate as cur_value,
