15b58e3ead2d23ed00e8b47bb0e4aeab3a0f53cd
Alex Stupak <itskills4practice@gmail.com>
2023-09-20 14:30:35 +0300

    PRSQL1-94: add value for procedure add_log

--- procedures/add_log.sql
+++ procedures/add_log.sql
@@ -1,8 +1,9 @@
-create procedure add_log(p_app_proc in varchar2,
+create or replace procedure add_log(p_app_proc in varchar2,
                          p_message in varchar2,
-                         p_log_date in date) is
+                         p_log_date in date,
+                         p_change_user in varchar2 default 'USER') is
 begin
-    insert into srv.logs (appl_proc, message, log_date)
-    values (p_app_proc, p_message, p_log_date);
+    insert into srv.logs (appl_proc, message, log_date, change_user)
+    values (p_app_proc, p_message, p_log_date, p_change_user);
     commit;
 end;
