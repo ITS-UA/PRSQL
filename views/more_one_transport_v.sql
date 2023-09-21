b41dd60e1ccc7af46800de44f4c2cce4f72575c1
ITS-UA <143839174+ITS-UA@users.noreply.github.com>
2023-09-21 16:52:59 +0300

    Update more_one_transport_v.sql

--- views/more_one_transport_v.sql
+++ views/more_one_transport_v.sql
@@ -1,13 +1,14 @@
-create view srv.more_one_transport_v1 as 
-  select cl.client_id,
+create view srv.more_one_transport_v as 
+select cl.client_id,
        cl.first_name,
        cl.last_name,
-       nvl(tmp.count_cars1, 0) as count_cars
+       tmp.count_cars1 as count_cars
 from srv.clients cl
 left outer join (select c.client_id, count(c.car_id) as count_cars1
             from srv.cars c
             group by c.client_id) tmp 
 on cl.client_id = tmp.client_id
+where tmp.count_cars1 >1
 order by cl.client_id asc;
 
 
