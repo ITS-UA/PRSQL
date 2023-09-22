b1d2e5cec87b57bc232f896e536b23a89189a9ee
ITS-UA <143839174+ITS-UA@users.noreply.github.com>
2023-09-22 17:48:34 +0300

    Update add_job.sql

--- procedures/add_job.sql
+++ procedures/add_job.sql
@@ -5,6 +5,8 @@ begin
 srv.check_work_time;
 
     case 
+    when p_min_salary > p_max_salary then
+      raise_application_error (-20002, 'мінімальна зарплата не повинна перевищувати максимальну зарплату');
     when p_min_salary < 5000 then
       raise_application_error (-20003,'розмір зарплати менший за 5000');
     when p_max_salary > 90000 then
